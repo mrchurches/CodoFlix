bb76be4a8bba568232d25e3cb2eb1eccf36d31ec
matiassg88 <127242855+matiassg88@users.noreply.github.com>
2024-04-16 02:37:27 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    let navbar = document.getElementById('navbar');
-    if (window.scrollY > 0 && !navbar.classList.contains('navbar_dark')) {
-        navbar.classList.add('navbar_dark');
-    } else if (window.scrollY === 0 && navbar.classList.contains('navbar_dark')) {
-        navbar.classList.remove('navbar_dark');
-    }
-});
